966312040ae2dce87c61f754e40e91805c369b24
clebergriff <clebergriff@gmail.com>
2020-10-03 18:10:44 -0300

    Agora foi

--- ReactJS/primeiro-projeto/src/components/comunicacao/api/ApiMagic2.jsx
+++ ReactJS/primeiro-projeto/src/components/comunicacao/api/ApiMagic2.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import './ApiMagic2.css';
 
 export default class ApiMagic2 extends React.Component {
 
     state = {
-      loading: true,
-      card: null,
-      search: null
+      card: null
     };
 
     async searchCard() {
@@ -13,24 +12,23 @@ export default class ApiMagic2 extends React.Component {
         const url = "https://api.scryfall.com/cards/search?order=cmc&q=" +this.state.value;
         const response = await fetch(url);
         const data = await response.json();        
-        this.setState({card: data, loading: true})
+        this.setState({card: data, loading: false});
     };
 
     constructor(props) {
       super(props);
-      this.state = {value: ''};
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {   
+      console.log(this.loading);
       this.setState({value: event.target.value});
     }
 
     handleSubmit(event) {
       this.searchCard()
-      alert('Um nome foi enviado: ' + this.state.value);
       event.preventDefault();
     }
   
@@ -39,24 +37,25 @@ export default class ApiMagic2 extends React.Component {
       return ( 
         <div>          
           <form onSubmit={this.handleSubmit}> 
-            <label>
-              Nome:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Enviar" />
+            <input type="text" placeholder="Informe a carta..."
+              onChange={this.handleChange} />
+            <input type="submit" value="Buscar" />
           </form>
           
-            {!this.state.loading ? (
-                <div> Loading...</div> 
-            ) : (                
+            {this.state.card == undefined ? (
+                <div> </div> 
+            ) : (  
+              !this.state.card.data ? (
+                <div className="title"> Nenhuma carta encontrada </div>
+              ) : (             
                 <div>
-                    <div>{this.state.card.data[0].name} </div>
+                    <div className="title">{this.state.card.data[0].name} </div>
                     <img alt="" 
-                        src={this.state.card.data[0].image_uris.large}>
+                        src={this.state.card.data[0].image_uris.normal}>
                     </img>
                 </div>
-            )}
-        </div>
+            ))}
+        </div>        
       );
     }
   }
