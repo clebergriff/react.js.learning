f4a944a8fc6d8995c2b9660ca9c4aff5b93e61bf
clebergriff <clebergriff@gmail.com>
2020-10-11 10:59:23 -0300

    Desmembramento de projeto

--- ReactJS/react.web.paginaamarela/src/components/layout/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Footer.css'
-
-export default props => 
-    <div className="Footer">
-        <h1> {props.children} </h1>
-    </div>
--- ReactJS/react.web.paginaamarela/src/components/layout/header/Header2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import './Header.css'
-
-export default props => 
-    <div className="Header">
-        <h1> {props.children} </h1>
-    </div>
